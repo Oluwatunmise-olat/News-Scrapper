dc7b0925d70051f908c87d2559b8e53038b3043f
Oluwatunmise <oluwatunmiseolatunbosun2001@gmail.com>
2021-10-06 02:23:25 +0100

    async setup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import asyncio
 from fastapi import FastAPI
 
 from src.routers import api_router
-from src.BaseClass.RedditApi import settings, Reddit
-from src.BaseClass.NewsApi import settings, NewsAPI
+from run_task import reddit_data, newsApi_data
 
 app = FastAPI()
 
@@ -11,16 +11,14 @@ app.include_router(api_router.router)
 
 # @app.on_event("startup")
 # def startup_event():
-""" Runs the following events before the application starts """
-# reddit_instance = Reddit(settings.CLIENT_ID, settings.SECRET_KEY)
-# print(reddit_instance.access_token)
-# print(reddit_instance.get_latest_news('fastapi', 1))
-#  'rediit': reddit_instance.get_latest_news('fastapi', 2)
-
-# newsapi_instance = NewsAPI(settings.NEWS_API_KEY)
-# print(newsapi_instance.params)
+#   pass
 
 
 @app.get("/name")
-def testing():
-    return {'data': settings.config}
+async def testing():
+    query = "fastapi"
+    limit = 3
+    reddit_result = await reddit_data(query, limit)
+    newsapi_result = await newsApi_data(query, limit)
+
+    return {'data': [reddit_result, newsapi_result]}
--- run_task.py
+++ run_task.py
@@ -1,1 +1,35 @@
-# import asyncio
+import asyncio
+from typing import Optional
+
+from src.BaseClass.RedditApi import settings, Reddit
+from src.BaseClass.NewsApi import settings, NewsAPI
+
+
+async def reddit_data(query, limit: Optional[int] = None):
+    # Instantiate class Instances
+    reddit_instance = Reddit(settings.CLIENT_ID, settings.SECRET_KEY)
+    if not limit:
+        requested_data = reddit_instance.get_latest_news(query)
+    requested_data = reddit_instance.get_latest_news(query, limit=limit)
+    return requested_data
+
+
+async def newsApi_data(query, limit: Optional[int] = None):
+    # Instantiate class Instances
+    newsapi_instance = NewsAPI(settings.NEWS_API_KEY)
+    if not limit:
+        requested_data = newsapi_instance.get_everything(query)
+    requested_data = newsapi_instance.get_everything(query, pagesize=limit)
+    return requested_data
+
+
+async def main(query, limit: Optional[int] = None):
+    reddit = loop.create_task(reddit_data(query=query, limit=limit))
+    newsapi = loop.create_task(newsApi_data(query=query, limit=limit))
+    await asyncio.wait([reddit, newsapi])
+    return reddit_result, newsapi_result
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    reddit_result, newsapi_result = loop.run_until_complete(main('asyncio', 2))
